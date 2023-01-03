b69c1885b694dd45e195cc820c941d1102dccdeb
Billy Alexander de la Cruz Duque <alexander.cruz.duque@gmail.com>
2023-01-03 16:12:22 -0600

    Pipes Personalizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/nest-pokemon?authSource=admin',
+    ),
+    PokemonModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
